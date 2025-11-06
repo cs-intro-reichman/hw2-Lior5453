ad0dac0cddaf813e5f159906faeac672e7054d35
Lior Rubin <lior.rubin@post.runi.ac.il>
2025-11-06 18:40:36 +0200

    Finished third exercise

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,26 @@
-//feedback
+public class Cheers
+{
+    public static void main(String[] args)
+    {
+        String inputWord = args[0];
+        int numPrints = Integer.parseInt(args[1]);
 
-/*
-Great work boaz!
-*/
+        String printA = "AEFHILMNORSX";
+        //{'A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R', 'S', 'X'}
 
-
-// Prints a crowd cheering output.
-public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+        for (int i = 0; i < inputWord.length(); i++)
+        {
+            char currentChar = inputWord.charAt(i);
+            if (printA.indexOf(currentChar) != -1)
+                System.out.printf("Give me an %c: %c!\n", currentChar, currentChar);
+            else
+                System.out.printf("Give me a  %c: %c!\n", currentChar, currentChar);
         }
-}
+
+        System.out.println("What does that spell?");
+        for (int i = 0; i < numPrints; i++)
+        {
+            System.out.printf("%s!!!\n", inputWord);
+        }
+    }
+}  
