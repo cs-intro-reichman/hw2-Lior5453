be78f38b537db1782e1eaa5b4b0cd8471de47f82
Lior Rubin <lior.rubin@post.runi.ac.il>
2025-11-08 09:42:42 +0200

    finished last exercise - checking c

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,57 @@
 // Demonstrates the Collatz conjecture.
-public class Collatz {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
+public class Collatz 
+{
+	public static void main(String args[]) 
+	{
+		int N = Integer.parseInt(args[0]); // Getting the seed
+		String mode = args[1];
+
+		if (mode.equals("v"))
+		{
+			for (int i = 1; i <= N; i++)
+			{
+				int currentNum = i;
+				int count = 1;
+				do // making to do while so it will check if cureentNum = 1
+				{
+					System.out.print(currentNum + " ");
+					if (currentNum % 2 == 0) 
+					{
+						currentNum /= 2;
+					}
+					else
+					{
+						currentNum = currentNum * 3 + 1;
+					}
+					count++;	
+				} while (currentNum != 1);
+				System.out.println("1 (" + count + ")");
+			}
+		}
+		else // "c" 
+		{
+			boolean reach1 = true;
+			
+			for (int i = 1; i <= N; i++)
+			{
+				int currentNum = i;
+				do
+				{
+					if (currentNum % 2 == 0) 
+					{
+						currentNum /= 2;
+					}
+					else
+					{
+						currentNum = currentNum * 3 + 1;
+					}
+				} while (currentNum != 1);
+			}
+
+			if (reach1)
+				System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+			else
+				System.out.println("An error was accured, 1 wasn't reached");
+		}
 	}
 }
